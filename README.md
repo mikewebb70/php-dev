10a7f7b1271a4b79b9a8d21b04c496fe03b55819
Michael Webb <mike@live.com.au>
2023-10-03 07:37:12 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A very basic docker nginx PHP-FPM and mysql development environment with sane defaults
+
+Amend Dockerfiles and docker-compose files to suit
---
+++ www/html/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+  <title>Hello World!</title>
+</head>
+
+<body>
+  <h1>Hello World!</h1>
+  <p><?php echo 'We are running PHP, verson: ' . phpversion(); ?></p>
+  <p><?php $defaultTimezone = date_default_timezone_get(); var_dump( $defaultTimezone ); ?></p>
+</body>
+</body>
